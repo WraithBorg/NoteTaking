30ec0b48538af8b33b5c07719fd868024628f6e2
WraithBorg <15620606954@139.com>
2018-11-08 16:05:21 +0800

    跳转页面

---
+++ app/src/main/java/com/zxu/admin/MainActivity.java
@@ -0,0 +1,33 @@
+package com.zxu.admin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.zxu.R;
+
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView noteOne = (TextView) findViewById(R.id.noteone_id);
+        noteOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noteOneMessage();
+            }
+        });
+    }
+
+    void noteOneMessage(){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+
+    }
+}
